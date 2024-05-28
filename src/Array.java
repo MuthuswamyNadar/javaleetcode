28cb66cd57ddb64f2b2ca4e7c1b4bcbb38a24c24
Swamy <muthuswamynadar25@gmail.com>
2024-05-28 09:03:00 +0530

    swamy

--- src/Array.java
+++ src/Array.java
@@ -1,5 +1,26 @@
 public class Array {
     public static void main(String[] args) {
-
+        //repo link
+        //https://github.com/MuthuswamyNadar/javaleetcode.git
+        // Reverse Array ( https://youtu.be/80auISlKy50?si=7FShi_mpAkxKIiDP )
+        int [] myarray={10,30,560,80,23};
+        revarray(myarray);
+        for(int i=0;i<myarray.length;i++)
+        {
+            System.out.print(myarray[i]+" ,");
+        }
+    }
+    static void revarray(int[] myarr)
+    {
+        int i=0;
+        int j=myarr.length-1;
+        while (i<j)
+        {
+            int temp=myarr[i];
+            myarr[i]=myarr[j];
+            myarr[j]=temp;
+            i++;
+            j--;
+        }
     }
 }
